9a04a40e8d858c247f181ebb274dcc36e4a63935
wellington <profwellington17@gmail.com>
2020-08-31 18:50:27 -0300

    exercicios 2

---
+++ modulo1/bloco_3/bloco_4/dia_3/ex2.js
@@ -0,0 +1,22 @@
+let num = 5;
+
+if(num >1){
+
+    for(let i=0; i <=num; i += 1){
+        let res = '';
+
+        for(let j=0; j<num; j += 1){
+            
+            
+            res += '*';
+            console.log(res);
+        }
+        
+     
+    }
+
+
+}else{
+
+    console.log("Erro: digite um valor maior que 1");
+}
